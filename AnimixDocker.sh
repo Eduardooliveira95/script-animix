2b48f6570d8747e10216ff87f027c22ab98baaff
Eduardooliveira95 <eduardo.oliveirafo@sptech.school>
2022-11-20 22:59:40 -0300

    Teste

--- AnimixDocker.sh
+++ AnimixDocker.sh
@@ -98,4 +98,5 @@ echo "Agora, vamos instalar o Container que conterá o java para executar uma ap
 sudo docker build -t dockerfile .
 sudo docker run -d -t --name containerjava dockerfile
 
-fi fi
+fi 
+fi
