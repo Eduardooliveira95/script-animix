e0b71b01938949af04609e6f700916b6549347dd
Eduardooliveira95 <eduardo.oliveirafo@sptech.school>
2022-11-27 20:56:10 -0300

    Teste

--- AnimixDocker.sh
+++ AnimixDocker.sh
@@ -25,7 +25,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker pull mysql:5.7
 sudo docker run -d -p 3306:3306 --name AnimixDocker -e "MYSQL_DATABASE=Animix" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
-sudo docker exec -it AnimixDocker mysql --protocol tcp -u root -p -e "
+sudo docker exec -it AnimixDocker mysql --protocol tcp -u root -p -B -N -e "
 
     use Animix;
 
