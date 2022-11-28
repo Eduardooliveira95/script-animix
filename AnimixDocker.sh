1b5941995c5e932a8610de45dd6f467d248feeea
Eduardooliveira95 <eduardo.oliveirafo@sptech.school>
2022-11-27 22:37:45 -0300

    Testte

--- AnimixDocker.sh
+++ AnimixDocker.sh
@@ -23,8 +23,8 @@ echo "Vamos fazer a instalação dos containers!"
 sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo docker pull mysql:5.7
-sudo docker run -d -p 3306:3306 --name AnimixDocker -e "MYSQL_DATABASE=animix" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+sudo docker pull mysql:8.0
+sudo docker run -d -p 3306:3306 --name AnimixDocker -e "MYSQL_DATABASE=animix" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:8.0
 sudo docker exec -it AnimixDocker mysql --protocol tcp -u root -p -B -N -e "
 
     use animix;
